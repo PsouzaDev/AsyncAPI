51cac840a5f68e6e6dbae75c5cb992feba922488
Pedro Souza <pesouza.civil@gmail.com>
2019-12-07 18:17:18 -0300

    first commit
    New project about Books with async methods

---
+++ Services/IBooksRepository.cs
@@ -0,0 +1,17 @@
+﻿using Books.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.API.Services
+{
+    interface IBooksRepository
+    {
+        Task<IEnumerable<Book>> GetBooksAsync();
+
+        Task<Book> GetBookAsync(Guid Id);
+
+
+    }
+}
